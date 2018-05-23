7482d2cc37e8717298056195609b5ab779d8216b
Jennapiro <39099694+Jennapiro@users.noreply.github.com>
2018-05-23 13:05:31 -0400

    Add files via upload

---
+++ employee.spec.js
@@ -0,0 +1,48 @@
+//import the Employee Class
+const Employee = require('./employee');
+
+describe("An Employee", () => {
+
+	it("has a first name", () => {
+		let Jenna = new Employee({ first_name: "Jenna" });
+		expect(Jenna.first_name).toBeDefined();
+		expect(Jenna.first_name).toBe("Jenna");
+	});
+
+	it("has a last name", () => {
+		let Jenna = new Employee({ last_name: "Piromalli" });
+		expect(Jenna.last_name).toBeDefined();
+		expect(Jenna.last_name).toBe("Piromalli");
+	});
+
+	it("has a position", () => {
+		let Jenna = new Employee({ first_name: "Jenna" , last_name: "Piromalli" , position: "Division Vice President" });
+		expect(Jenna.position).toBeDefined();
+		expect(Jenna.position).toBe("Division Vice President")
+	});
+
+
+	it("has meetings", () => {
+		let Jenna = new Employee({ first_name: "Jenna" , last_name: "Piromalli" });
+		expect(Jenna.meetings).toBeDefined();
+		expect(Array.isArray(Jenna.meetings)).toBe(true);
+	});
+
+	it ("can schedule a meeting", () => {
+		let Jenna = new Employee({ first_name: "Jenna", last_name: "Piromalli" });
+
+	//create a fake meeting
+		let mock10 = {
+		time: "10",
+		location: "clients office",
+		client_company_name: "IBM",
+	};
+
+		let num_scheduled = Jenna.meetings.length;
+		Jenna.schedule(mock10);
+		expect(Jenna.meetings.length).toBe(num_scheduled +1);
+		expect(Jenna.meetings[num_scheduled].time).toBe("10");
+		expect(Jenna.meetings[num_scheduled].location).toBe("clients office");
+	});
+});
+
