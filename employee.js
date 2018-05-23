6e524af8e7b6725970fcf90f334c3b9f5e2f5881
Jennapiro <39099694+Jennapiro@users.noreply.github.com>
2018-05-23 12:50:48 -0400

    Add files via upload

---
+++ employee.js
@@ -0,0 +1,14 @@
+// Here is where we'll implement
+//the student class
+
+function Employee(props) {
+	this.first_name = props.first_name || "";
+	this.last_name = props.last_name || "";
+	this.position = props.position || "";
+	this.meetings = [];
+	this.schedule =function(meeting) {
+		this.meetings.push(meeting);
+	};
+}
+
+module.exports = Employee;
