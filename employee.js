206c086a40b02527f980bb4b41f2780b34bd6991
Jennapiro <39099694+Jennapiro@users.noreply.github.com>
2018-05-29 10:26:16 -0400

    Add files via upload

--- employee.js
+++ employee.js
@@ -9,6 +9,18 @@ function Employee(props) {
 	this.schedule = function(meeting) {
 		this.meetings.push(meeting);
 	};
+	var number_of_meetings_per_month;
+	if (number_of_meetings_per_month < 10) {
+		console.log ("in a bad company standing");
+	}
+	if (number_of_meetings_per_month < 19){
+		console.log ("in an average company stadning");
+	}
+	if (number_of_meetings_per_month < 30){
+		console.log ("in a competitive company standing");
+	}
+	else if (number_of_meetings_per_month == 0){
+		console.log ("need to meet with your boss");
+	}
 }
-
 module.exports = Employee;
